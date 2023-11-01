6058a34e4a1117c00e7e23d1b2f3c4e8c7b10bbd
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-01 15:57:26 +0000

    searchquery

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 8080;
 const app = express();
+const PORT = process.env.PORT || 8080;
 app.use(cors());
+const axios = require("axios");
 
-// add your endpoints here
+const data = require("./data/weather.json");
 
-app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
+app.get("/", (_, response) => response.json("Root route."));
+
+app.get("/weather", (request, response) => {
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  const searchQuery = request.query.searchQuery;
+
+  const filteredCity = data.find((city) => {
+    return (
+      city.city_name === searchQuery && city.lat === lat && city.lon === lon
+    );
+  });
+
+  const wrangledData = filteredCity.data.map((day) => {
+    return {
+      description: day.weather.description,
+      date: day.datetime,
+    };
+  });
+  response.json(wrangledData);
+});
+
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+
+/*request.query = 
+{lat:10,
+lon:12,
+searchQuery: "Paris"}; */
